3a58373b2b9df69bafa54e5727fdf6f49a40889c
Robin Schnetler <robin.schnetler@entelect.co.za>
2017-09-25 13:48:53 +0200

    Setup recipe output

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'http://ichef.bbci.co.uk/food/ic/food_16x9_506/recipes/chicken_pasta_bake_25701_16x9.jpg')
+  ];
 
   constructor() { }
 
